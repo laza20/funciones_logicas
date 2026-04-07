6a0072d48fe61915645e4d3d11b51c26c76cc3a4
lazaro20 <salvirialazaro@gmail.com>
2026-04-07 16:17:29 -0300

    Feat: Actualizar la funcion para inicializar un arbol binario.

--- nodos/inicializar_nodos.py
+++ nodos/inicializar_nodos.py
@@ -3,16 +3,27 @@ class Nodo:
         self.valor = valor
         self.next = None
         
+class NodoArbol:
+    def __init__(self, valor):
+        self.valor = valor
+        self.left = None 
+        self.right = None
         
-def cuatro_nodos_return_head():
-    a = Nodo(5)
-    b = Nodo(9)
-    c = Nodo(90)
-    d = Nodo(77)
-    a.next = b
-    b.next = c
-    c.next = d
-    return a
+def nodos_con_hijos_bts():
+    raiz = NodoArbol(10)
+
+    raiz.left = NodoArbol(8)
+    raiz.left.left = NodoArbol(6)
+    raiz.left.right = NodoArbol(1456)
+    raiz.left.left.left = NodoArbol(3)
+    raiz.left.left.right = NodoArbol(7)
+
+    raiz.right = NodoArbol(12)
+    raiz.right.left = NodoArbol(11)       
+    raiz.right.right = NodoArbol(15)
+    raiz.right.right.left = NodoArbol(13)
+    raiz.right.right.right = NodoArbol(20)
+    return raiz
 
 
 def cuatro_nodos_return_head():
